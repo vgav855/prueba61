faf66e1d3c1a4f2b76fc7075d4e27a7a641944f1
vgav855 <132547267+vgav855@users.noreply.github.com>
2023-05-05 12:12:56 +0200

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,7 @@
-
+basic.showString("Hola")
+for (let index = 0; index < 5; index++) {
+    basic.showIcon(IconNames.Heart)
+    basic.pause(100)
+    basic.showIcon(IconNames.SmallHeart)
+    basic.pause(100)
+}
